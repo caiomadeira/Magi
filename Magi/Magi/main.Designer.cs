8ba3029d7ec9955385a60d2166e2bc0c69414c23
Caio Madeira <51931284+caiomadeira@users.noreply.github.com>
2023-06-19 01:20:54 -0700

    initial files

---
+++ Magi/Magi/main.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Magi
+{
+    partial class main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            loginButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(157, 135);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(255, 128, 0);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(535, 256);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 18);
+            textBox1.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            loginButton.FlatStyle = FlatStyle.Popup;
+            loginButton.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            loginButton.ForeColor = Color.Coral;
+            loginButton.Location = new Point(535, 285);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(211, 23);
+            loginButton.TabIndex = 2;
+            loginButton.Text = ">Enter";
+            loginButton.TextAlign = ContentAlignment.BottomLeft;
+            loginButton.UseVisualStyleBackColor = true;
+            loginButton.Click += loginButton_Click;
+            // 
+            // main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(loginButton);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "main";
+            Text = "Magi";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button loginButton;
+    }
+}
---
+++ Magi/Magi/main.cs
@@ -0,0 +1,15 @@
+namespace Magi
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
